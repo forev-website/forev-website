f81febd9a83a9996b633dff60beb846abe29563a
Halil Sarıca <info@forevtekstil.com>
2026-09-03 00:48:42 +0300

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import { Manrope, Geist } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -24,6 +28,22 @@ export default function RootLayout({
     <html lang="tr" className={cn("font-sans", geist.variable)}>
       <body className={manrope.className}>
         {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HSPZVHQRQV"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HSPZVHQRQV');
+          `}
+        </Script>
       </body>
     </html>
   );
